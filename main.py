f365376702a3d52e2db58f768c9c8ef0dca994ff
DawidGorny01 <163434766+DawidGorny01@users.noreply.github.com>
2024-04-16 17:17:04 +0200

    wip: update data structure to list of dict

--- main.py
+++ main.py
@@ -3,13 +3,8 @@ users: list = [
     {"name": "Julia", "surname": "Gotowiec", "posts": 1500, },
     {"name": "Hubert", "surname": "Sybilski", "posts": 123456, },
     {"name": "Adrian", "surname": "Dobrzański", "posts": 3, }
-
 ]
 
-def read_friends(users: list)->None:
-    print("informacje o twoich znajomych: ")
-    for user in users:
-        print(f'\tTwój znajomy {user["name"]} {user["surname"]} opublikował {user["posts"]} postów.')
-
-
-read_friends(users)
+print("informacje o twoich znajomych: ")
+for user in users:
+    print(f'\tTwój znajomy {user["name"]} {user["surname"]} opublikował {user["posts"]} postów.')
